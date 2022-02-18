70240f31ea623dcbf21eefd88893e0de5ff51326
Valentine Garbuzenko <nickf@tut.by>
2022-02-18 21:44:41 +0300

    feat: create a chunk10 to Mcode function

--- src/index.js
+++ src/index.js
@@ -48,16 +48,41 @@ function decode(expr) {
 
 const toChunks10 = data => {
   const chunks10 = [];
-  console.log(data, data.length);
+  // console.log(data, data.length);
   for (let i = 0; i < data.length; i += 10) {
-    console.log(chunks10);
     chunks10.push(data.slice(i, i + 10));
+    // console.log(chunks10);
   }
   return chunks10;
 };
 
-res = toChunks10(expr);
-console.log();
+let res = toChunks10(expr);
+console.log(res);
+
+const chunkToMcode = chunk10 => {
+  let inStr = chunk10;
+  const subStr = '00'
+  while (inStr.includes(subStr)) {
+    inStr = inStr.replace(subStr, '');
+    // console.log(subStr, inStr); 
+  }
+  const subStrDot = '10'
+  while (inStr.includes(subStrDot)) {
+    inStr = inStr.replace(subStrDot, '.');
+    // console.log(subStrDot, inStr); 
+  }
+  const subStrDash = '11'
+  while (inStr.includes(subStrDash)) {
+    inStr = inStr.replace(subStrDash, '-');
+    console.log(subStrDash, inStr); 
+  }
+  return inStr;
+};
+
+for(let i = 0; i < res.length; i++) {
+  res[i] = chunkToMcode(res[i]);
+}
+console.log(res);
 
 /* 
 module.exports = {
